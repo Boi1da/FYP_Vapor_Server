bb7c9e3d3554ac57a7e37e9bfe5dad03f1880bba
Armani Cesar <armanicesar123@live.com>
2018-05-31 14:37:57 +0100

    no message

---
+++ Sources/Run/Scripts.swift
@@ -0,0 +1,69 @@
+//
+//  Scripts.swift
+//  pokemon
+//
+//  Created by Armani on 16/04/2018.
+//
+
+import Foundation
+
+
+class Scripts {
+    
+    @objc dynamic var isRunning = false
+    var resultsString : String!
+    var buildTask : Process!
+    var outputPipe : Pipe!
+    var duplicatedLinesCount : Int!
+    var complexityLinesCount : Int!
+    
+    //Duplication!
+    func startTask(pmdFilePath : String,minTokens : String, projectPath : String, language : String, fileFormat : String, ruleSetPath : String ){
+        //Append parameters into array
+        var arguments:[String] = []
+        arguments.append(minTokens)
+        arguments.append(projectPath)
+        arguments.append(language)
+        arguments.append(pmdFilePath)
+        arguments.append(fileFormat)
+        arguments.append(ruleSetPath)
+        
+        print("The parameters for our arguments are \(arguments)")
+        runScript(arguments)
+    }
+
+    func runScript(_ arguments:[String]){
+        resultsString = String()
+        isRunning = true
+        let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+        taskQueue.async {
+            self.buildTask = Process()
+            self.buildTask.launchPath = "/Users/Armani/pokemon/RunPMD.command"
+        
+            self.buildTask.arguments = arguments
+            self.buildTask.terminationHandler = {
+                task in
+                DispatchQueue.main.async(execute: {
+                    self.isRunning = false
+                })
+            }
+            let pipe = Pipe()
+            self.buildTask.standardOutput = pipe
+            self.buildTask.launch()
+            
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+            print(output)
+            
+            
+            self.resultsString = output
+            
+            //Check to see if the sentence contains a number!
+            print(self.resultsString)
+            
+            self.buildTask.waitUntilExit()
+        }
+    }
+}
+
+
